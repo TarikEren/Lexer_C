db87044083836887eff04287ffe6f517f03c01ec
TarikEren <1210606015@nku.edu.tr>
2025-12-31 17:11:38 +0300

    Added custom error codes

---
+++ headers/errorCodes.h
@@ -0,0 +1,18 @@
+#ifndef ERROR_CODES_H
+#define ERROR_CODES_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+#define SUCCESS                     0
+#define FAILURE                     1
+#define INVALID_ARRAY_ALLOCATION    2
+#define INVALID_BUFFER_ALLOCATION   3
+#define INVALID_CHARACTER           4
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif // ERROR_CODES_H
